1ea873958a324f63405e9407c3c5e6703809e23f
Nick Shvelidze <shveloo@gmail.com>
2015-08-19 18:59:17 +0400

    Add bus list

--- TileUpdater/Updater.cs
+++ TileUpdater/Updater.cs
@@ -7,22 +7,55 @@ using Windows.ApplicationModel.Background;
 using Windows.UI.Notifications;
 using Windows.Data.Xml.Dom;
 using System.Diagnostics;
+using System.Threading;
 
 namespace TileUpdater
 {
     public sealed class Updater : IBackgroundTask
     {
+        private XmlDocument tileXml;
+        private XmlElement tileText;
+        private Bus[] buses;
+
+        public struct Bus
+        {
+            public string name;
+            public int time;
+
+            public Bus(string name, int time) {
+                this.name = name;
+                this.time = time;
+            }
+        }
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             Debug.WriteLine("Background task starting");
 
-            var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text03);
+            tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text04);
 
-            var tileText = tileXml.GetElementsByTagName("text");
-            (tileText[0] as XmlElement).InnerText = "Row 0";
-            (tileText[1] as XmlElement).InnerText = "Row 1";
-            (tileText[2] as XmlElement).InnerText = "Row 2";
-            (tileText[3] as XmlElement).InnerText = "Row 3";
+            tileText = tileXml.GetElementsByTagName("text")[0] as XmlElement;
+
+            buses = new Bus[]
+            {
+                new Bus("99", 3),
+                new Bus("88", 7),
+                new Bus("6", 16),
+                new Bus("47", 21),
+            };
+
+            new Timer(onTimer, new object(), 0, 1000);
+        }
+
+        private void onTimer(object state)
+        {
+            tileText.InnerText = "";
+
+            foreach (Bus bus in buses)
+            {
+                tileText.InnerText += String.Format("%s: %02d", bus.name, bus.time);
+                //if (bus.time > 0) bus.time--;
+            }
             var tileNotification = new TileNotification(tileXml);
             var tileUpdater = TileUpdateManager.CreateTileUpdaterForApplication();
             tileUpdater.Update(tileNotification);
